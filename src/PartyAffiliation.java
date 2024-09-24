faaaf43b0763dcf221f44bf601def3b448a4826a
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-24 08:27:33 -0400

    fin com

---
+++ src/PartyAffiliation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PartyAffiliation {
+    public static void main(String[] args) {
+
+        String userParty;
+
+        System.out.println("Hello - are you Democrat, Republican, or Independent? Please type in one of the following: D, R, I");
+
+        Scanner scan = new Scanner (System.in);
+
+        if (scan.hasNextLine()) {
+            userParty = scan.nextLine();
+            if (userParty.equals("D")) {
+                System.out.println("You are a Democrat, who's mascot is a Donkey.");
+            } else if (userParty.equals("R")) {
+                System.out.println("You are a Republican, who's mascot is a Elephant.");
+            } else if (userParty.equals("I")) {
+                System.out.println("You are Independent, who's mascot is a Person.");
+            } else {
+                System.out.println("You have inputted an invalid response.");
+                System.exit(0);
+            }
+        } else {
+        System.out.println("You have inputted an invalid response.");
+        System.exit(0);
+        }
+
+    }
+}
